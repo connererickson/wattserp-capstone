2bf8464127e1a81a0b8c73c3b7a7728851c2ad22
Eric Waters <eswaters@asu.edu>
2021-11-21 13:05:43 -0700

    dashboard changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,16 +14,14 @@
 	    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
 	    <link href="{{ URL::asset('css/messi.css') }}" rel="stylesheet">
 	    <link href="{{ URL::asset('css/fonts.css') }}" rel="stylesheet">
-	    <script src="{{ URL::asset('js/app.js') }}"></script>
 	    <link href="{{ URL::asset('css/fontawesome.min.css') }}" rel="stylesheet">
 	    <link href="{{ URL::asset('css/all.min.css') }}" rel="stylesheet">
 	    <link href="{{ URL::asset('css/generaladmin.css') }}" rel="stylesheet">
 	    <link href="{{ URL::asset('css/module_styles.css') }}" rel="stylesheet">
 	    <link href="{{ URL::asset('css/form_builder.css ') }}" rel="stylesheet">
-		<script src="../../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-		<script src="../../node_modules/jquery/dist/jquery.min.js"></script>
-		<!-- <script src="/js/jquery.min.js"></script>
-    	<script src="/js/bootstrap.min.js"></script> -->
+
+		{{-- script --}}
+		<script src="{{ URL::asset('js/app.js') }}"></script>
 	</head>
 	<body>
 		<div id='modal_dialog_update_background'></div>
@@ -32,169 +30,148 @@
 			<input type='button' id='close_modal_dialog' class='crm_btn' value='OK' />
 		</div>
 	    <div id="app">
-	        <nav class="navbar navbar-expand-lg navbar-light bg-light" id="erp_main_nav">
-	        <!-- <nav class="navbar navbar-default navbar-static-top" id="erp_main_nav"> -->
-	            <div class="container">
-	                <div class="navbar-header">
+	        <nav class="navbar navbar-expand-xl navbar-light bg-light" id="erp_main_nav">
+	            <div class="container-fluid">
+					<img src="{{ URL::asset( 'storage/' . $org_dir . '/logo.png' ) }}" alt="" width="225" height="85" class="d-inline-block align-text-top">
+					{{-- <img src="{{ URL::asset( 'storage/' . $org_dir . '/logo.png' ) }}" alt='' class="img-fluid" style="max-width: 30%"/> --}}
+					<button class="navbar-toggler border" type="button" data-bs-toggle="collapse" data-bs-target="#app-navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+				
+				<div class="collapse navbar-collapse" id="app-navbar-collapse">
+					<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+						<li class="nav-item"><a href="{{ route('dashboard') }}" class="nav-link">Dashboard</a></li>
+						@if (Auth::user()->authorizeRoles(array('Sales')))
+							<li class="nav-item"><a href="{{ route('resources') }}">Resources</a></li>
+							<li class="nav-item"><a href="{{ route('profile') }}">My Profile</a></li>
+						@endif
+						@if (Auth::user()->authorizeRoles(array('Internal')))
+						<li class="nav-item dropdown">
+							<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
+								Human Resources
+							</a>
+							<ul class="dropdown-menu" role="menu">
+								<li>
+									<a href="{{ route('employees') }}" class="dropdown-item">Employees</a>
+								</li>
+								@if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Field Coordinator', 'Human Resources', 'Safety')))
+								<li>
+									<a href="{{ route('safety') }}" class="dropdown-item">Safety</a>
+								</li>
+								@endif
+								@if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Sales Manager', 'Human Resources', 'Contracts')))
+								<li>
+									<a href="{{ route('channel_partners') }}" class="dropdown-item">Channel Partners</a>
+								</li>
+								@endif
+							</ul>
+						</li>
+						@endif
+						@if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Contracts', 'Field Lead', 'Field Coordinator', 'Project Manager', 'Field Crew')))
+						<li class="nav-item dropdown">
+							<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
+								Project
+							</a>
+							<ul class="dropdown-menu" role="menu">
+								@if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Contracts', 'Project Manager')))
+								<li class="nav-item">
+									<a href="{{ route('leads') }}" class="dropdown-item">Leads</a>
+								</li>
+								@endif
+								<li class="nav-item">
+									<a href="{{ route('projects') }}" class="dropdown-item">Projects</a>
+								</li>
+								<li class="nav-item">
+									<a href="{{ route('archive') }}" class="dropdown-item">Archive</a>
+								</li>
+							</ul>
+						</li>
+						@endif
+						@if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Contracts', 'Field Lead', 'Field Coordinator', 'Project Manager', 'Inventories', 'Safety')))
+						<li class="nav-item dropdown">
+							<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
+								Inventory 
+							</a>
+							<ul class="dropdown-menu" role="menu">
+								
+								<li class="nav-item">
+									<a href="{{ route('inventory') }}" class="dropdown-item">Inventory</a>
+								</li>
+								@if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Contracts', 'Project Manager', 'Inventories', 'Safety')))
+								<li class="nav-item">
+									<a href="{{ route('repository') }}" class="dropdown-item">Repository</a>
+								</li>
+								@endif
+							</ul>
+						</li>
+						@endif
+						@if (Auth::user()->authorizeRoles(array('Internal', 'Sales')))
+							<li class="nav-item">
+								<a href="{{ route('directory') }}" class="nav-link">Directory</a>
+							</li>
+						@endif
+						@if (Auth::user()->authorizeRoles(array('Internal')))
+						<li class="nav-item dropdown">
+							<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
+								Applications
+							</a>
+							<ul class="dropdown-menu" role="menu">
+								
+								<li class="nav-item">
+									<a href="{{ route('weather') }}" class="dropdown-item">Weather Station</a>
+								</li>
+							</ul>
+						</li>
+						@endif
+						<!-- Authentication Links -->
+						@if (Auth::guest())
+							<li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
+							<!--<li><a href="{{ route('register') }}">Register</a></li>-->
+						@else
+							@if (Auth::user()->authorizeRoles(array('Super Admin')))
+							<li class="nav-item dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
+									Switch Organization
+								</a>
+								<ul class="dropdown-menu" role="menu">
+									@foreach ($all_orgs as $organization)
+										<li><a href="{{ route('dashboard', ['org_id' => $organization->id]) }}" class="dropdown-item">{{ $organization->name }}</a></li>
+									@endforeach
+								</ul>
+							</li>
+							@endif
+							<li class="nav-item dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
+									{{ Auth::user()->name }}
+								</a>
 
-					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#app-navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                		<span class="navbar-toggler-icon"></span>
-           			</button>
-	                    <!-- Collapsed Hamburger
-	                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-	                        <span class="sr-only">Toggle Navigation</span>
-	                        <span class="icon-bar"></span>
-	                        <span class="icon-bar"></span>
-	                        <span class="icon-bar"></span>
-	                    </button> -->
-	
-	                    <!-- Branding Image -->
-	                    <!--<a class="navbar-brand" href="{{ url('/') }}">-->
-	                    <img src="{{ URL::asset( 'storage/' . $org_dir . '/logo.png' ) }}" alt='' />
-	                    <!--</a>-->
-	                </div>
-	
-	                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-	                    <!-- Left Side Of Navbar -->
-	                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-	                        <li class="nav-item"><a href="{{ route('dashboard') }}" class="nav-link">Dashboard</a></li>
-	                        @if (Auth::user()->authorizeRoles(array('Sales')))
-	                           <li class="nav-item"><a href="{{ route('resources') }}">Resources</a></li>
-	                           <li class="nav-item"><a href="{{ route('profile') }}">My Profile</a></li>
-	                        @endif
-	                        @if (Auth::user()->authorizeRoles(array('Internal')))
-	                        <li class="nav-item dropdown">
-	                        	<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
-	                            	Human Resources
-									<!-- <span class="caret"></span> -->
-	                            </a>
-	                            <ul class="dropdown-menu" role="menu">
-	                                <li>
-	                                	<a href="{{ route('employees') }}" class="dropdown-item">Employees</a>
-	                                </li>
-	                                @if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Field Coordinator', 'Human Resources', 'Safety')))
-	                                <li>
-	                                	<a href="{{ route('safety') }}" class="dropdown-item">Safety</a>
-	                                </li>
-	                                @endif
-	                                @if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Sales Manager', 'Human Resources', 'Contracts')))
-	                                <li>
-	                                	<a href="{{ route('channel_partners') }}" class="dropdown-item">Channel Partners</a>
-	                                </li>
-	                                @endif
-	                        	</ul>
-	                        </li>
-	                        @endif
-	                        @if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Contracts', 'Field Lead', 'Field Coordinator', 'Project Manager', 'Field Crew')))
-	                        <li class="nav-item dropdown">
-	                        	<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
-	                            	Project <span class="caret"></span>
-	                            </a>
-	                            <ul class="dropdown-menu" role="menu">
-	                            	@if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Contracts', 'Project Manager')))
-	                                <li class="nav-item">
-	                                	<a href="{{ route('leads') }}" class="nav-link">Leads</a>
-	                                </li>
-	                                @endif
-	                                <li class="nav-item">
-	                                	<a href="{{ route('projects') }}" class="nav-link">Projects</a>
-	                                </li>
-	                                <li class="nav-item">
-	                                	<a href="{{ route('archive') }}" class="nav-link">Archive</a>
-	                                </li>
-	                        	</ul>
-	                    	</li>
-	                    	@endif
-	                    	@if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Contracts', 'Field Lead', 'Field Coordinator', 'Project Manager', 'Inventories', 'Safety')))
-	                    	<li class="nav-item dropdown">
-	                        	<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
-	                            	Inventory <span class="caret"></span>
-	                            </a>
-	                            <ul class="dropdown-menu" role="menu">
-	                            	
-	                            	<li class="nav-item">
-	                                	<a href="{{ route('inventory') }}" class="nav-link">Inventory</a>
-	                                </li>
-	                                @if (Auth::user()->authorizeRoles(array('Administrator', 'Executive', 'Contracts', 'Project Manager', 'Inventories', 'Safety')))
-	                                <li class="nav-item">
-	                                	<a href="{{ route('repository') }}" class="nav-link">Repository</a>
-	                                </li>
-	                                @endif
-	                        	</ul>
-	                    	</li>
-	                    	@endif
-	                    	@if (Auth::user()->authorizeRoles(array('Internal', 'Sales')))
-	                    		<li class="nav-item">
-	                    			<a href="{{ route('directory') }}" class="nav-link">Directory</a>
-	                    		</li>
-	                    	@endif
-	                    	@if (Auth::user()->authorizeRoles(array('Internal')))
-	                    	<li class="nav-item dropdown">
-                                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
-                                    Applications <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu" role="menu">
-                                    
-                                    <li class="nav-item">
-                                        <a href="{{ route('weather') }}" class="nav-link">Weather Station</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            @endif
-	                    </ul>
-	
-	                    <!-- Right Side Of Navbar -->
-	                    <ul class="nav navbar-nav navbar-right">
-	                        <!-- Authentication Links -->
-	                        @if (Auth::guest())
-	                            <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
-	                            <!--<li><a href="{{ route('register') }}">Register</a></li>-->
-	                        @else
-	                        	@if (Auth::user()->authorizeRoles(array('Super Admin')))
-	                        	<li class="nav-item dropdown">
-	                                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
-	                                    Switch Organization <span class="caret"></span>
-	                                </a>
-	                                <ul class="dropdown-menu" role="menu">
-	                                	@foreach ($all_orgs as $organization)
-	                                		<li><a href="{{ route('dashboard', ['org_id' => $organization->id]) }}" class="nav-link">{{ $organization->name }}</a></li>
-	                                	@endforeach
-	                                </ul>
-	                            </li>
-	                            @endif
-	                            <li class="nav-item dropdown">
-	                                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
-	                                    {{ Auth::user()->name }} <span class="caret"></span>
-	                                </a>
-	
-	                                <ul class="dropdown-menu" role="menu">
-	                                	@if (Auth::user()->authorizeRoles('Administrator'))
-		                            		<li class="nav-item">
-		                                    	<a href="{{ route('admin') }}" class="nav-link">Admin</a>
-		                                	</li>
-		                                @endif
-		                                <li class="nav-item">
-		                                	<a href="{{ route('settings') }}" class="nav-link">My Settings</a>
-		                                </li>
-	                                    <li class="nav-item">
-	                                        <a href="{{ route('logout') }}"
-	                                            onclick="event.preventDefault();
-	                                                     document.getElementById('logout-form').submit();" class="nav-link">
-	                                            Logout
-	                                        </a>
-	
-	                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-	                                            {{ csrf_field() }}
-	                                        </form>
-	                                    </li>
-	                                </ul>
-	                    		</li>
-	                        @endif
-	                    </ul>
-	                </div>
+								<ul class="dropdown-menu" role="menu">
+									@if (Auth::user()->authorizeRoles('Administrator'))
+										<li class="nav-item">
+											<a href="{{ route('admin') }}" class="dropdown-item">Admin</a>
+										</li>
+									@endif
+									<li class="nav-item">
+										<a href="{{ route('settings') }}" class="dropdown-item">My Settings</a>
+									</li>
+									<li class="nav-item">
+										<a href="{{ route('logout') }}"
+											onclick="event.preventDefault();
+														document.getElementById('logout-form').submit();" class="dropdown-item">
+											Logout
+										</a>
+
+										<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+											{{ csrf_field() }}
+										</form>
+									</li>
+								</ul>
+							</li>
+						@endif
+					</ul>
 	            </div>
 	        </nav>
-	
+			
 	        @yield('content')
 	    </div>
 	
@@ -209,6 +186,8 @@
 	    <script src="{{ URL::asset('/js/modules.js' ) }}"></script>
 	    <script src="{{ URL::asset('/js/ajax_functions.js' ) }}"></script>
 	    <script src="{{ URL::asset('/js/form_builder.js' ) }}"></script>
+		<script src="../../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="../../node_modules/jquery/dist/jquery.min.js"></script>
 	    <script type='text/javascript'>
 	    	$('.phone').text(function(i, text) {
 			    return text.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,62 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-	<style type='text/css'>
-		
-	</style>
-    <div class="row">
-        <div class="border py-2 px-2 col-md-12 text-center">
-            <div class="mt-2">
-				<h2>Dashboard</h2>
-				<div class="text-right">
-					<h3>Install Dashboard Modules</h3>
+<div class="container-fluid">
+	<div class="border border-primary border-2 rounded p-4 col-md-12 text-center">
+		<div class="mt-2">
+			<h2>Dashboard</h2>
+			<div>
+				<h5 class="text-muted">Install Dashboard Modules</h5>
+				<div class="d-flex justify-content-center flex-column flex-sm-row">
 					<?php
-            			if ($available_modules){ ?>
-            				<select id='select_dmodules' class="form-select my-2"><?php
-                				foreach($available_modules as $available_module){ ?>
-                    				<option value='<?php echo $available_module['id'] ?>'><?php echo $available_module['name'] ?></option><?php
-                    			}?>
-							 </select>
-							 <button id='install_dmodule' class='btn btn-success my-2'>Install</button><?php
-                    	 	}
-							 //crm_btn
-                    	else{
-                    		?><p>No modules available</p><?php
-                    	}?>
-						<br class='clear_fix' />
-				</div>
+					if ($available_modules){ ?>
 
-				
-                <div class="panel-body mt-2">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    
-                    <!-- GET DASHBOARD MODULES FOR USER -->
-                    <div class="grid" id='dashboard_grid'>
-                    	<div class="grid-sizer"></div>
-					  	<?php 
-                    	$filename = '';
-						$counter = 1;
-                    	if($installed_modules){
-                    		foreach($installed_modules as $installed_module){
-	                    		//var_dump($modules_data);
-	                    		$filename = $installed_module['filename'];?>
-						
-						  		<div class="grid-item" data-item-id="{{ $counter }}" id='<?php echo $filename; ?>'>
-									@include('dashboard_modules.' . $filename)
-						  		</div>
-						  		<?php $counter++; ?>
-						  	<?php
-						  	}
-						}?>
+							<select id='select_dmodules' class="form-select my-2 w-100 w-sm-25 text-center me-2"><?php
+								foreach($available_modules as $available_module){ ?>
+									<option value='<?php echo $available_module['id'] ?>' class="my-1"><?php echo $available_module['name'] ?></option><?php
+								}?>
+							</select>
+
+
+							<button id='install_dmodule' class='btn btn-primary my-2'>Install</button>
+
+						<?php
+						}
+							//crm_btn
+					else{
+						?><p>No modules available</p><?php
+					}?>
+					{{-- <br class='clear_fix' /> --}}
+				</div>
+			</div>
+
+			
+			<div class="panel-body mt-2">
+				@if (session('status'))
+					<div class="alert alert-success">
+						{{ session('status') }}
 					</div>
-                </div>
-            </div>
-        </div>
-    </div>
+				@endif
+				
+				<!-- GET DASHBOARD MODULES FOR USER -->
+				<div class="grid" id='dashboard_grid'>
+					<div class="grid-sizer"></div>
+					<?php 
+					$filename = '';
+					$counter = 1;
+					if($installed_modules){
+						foreach($installed_modules as $installed_module){
+							//var_dump($modules_data);
+							$filename = $installed_module['filename'];?>
+					
+							<div class="grid-item" data-item-id="{{ $counter }}" id='<?php echo $filename; ?>'>
+								@include('dashboard_modules.' . $filename)
+							</div>
+							<?php $counter++; ?>
+						<?php
+						}
+					}?>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
