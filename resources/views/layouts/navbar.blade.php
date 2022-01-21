c39b48437ff1570691b50c39cae93e34ced66bc8
Eric Waters <eswaters@asu.edu>
2022-01-20 20:39:50 -0700

    fixed a couple things

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -184,8 +184,8 @@
 	    <script src="{{ URL::asset('/js/modules.js' ) }}"></script>
 	    <script src="{{ URL::asset('/js/ajax_functions.js' ) }}"></script>
 	    <script src="{{ URL::asset('/js/form_builder.js' ) }}"></script>
-		<script src="../../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-		<script src="../../node_modules/jquery/dist/jquery.min.js"></script>
+		<script src="{{ URL::asset('../node_modules/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+		<script src="{{ URL::asset('../node_modules/jquery/dist/jquery.min.js')}}"></script>
 	    <script type='text/javascript'>
 	    	$('.phone').text(function(i, text) {
 			    return text.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
