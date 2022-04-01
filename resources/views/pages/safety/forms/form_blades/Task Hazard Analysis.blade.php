5dd8ca957a6b4afab86f43297c05d693e8d1cab7
CollinWood12 <cjwood2000@gmail.com>
2022-03-31 20:56:18 -0700

    some forms

--- resources/views/pages/safety/forms/form_blades/Task Hazard Analysis.blade.php
+++ resources/views/pages/safety/forms/form_blades/Task Hazard Analysis.blade.php
@@ -18,9 +18,149 @@
                     @endif
                     
                     
+                    
                     <div class="container">
-                        Form content here
-                    </div>
+                    <h1>
+                        Task Hazard Analysis
+                    </h1>
+                    <form>
+                        <div class="mb-3">
+                        <label for="taskLabel" class="form-label">Task:</label>
+                        <input type="text" class="form-control" id="taskLabel" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="projectLabel" class="form-label">Project:</label>
+                            <input type="text" class="form-control" id="projectLabel" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="managerLabel" class="form-label">Project Manager:</label>
+                            <input type="text" class="form-control" id="managerLabel" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="dateLabel" class="form-label">Date</label>
+                            <input type="date" class="form-control" id="dateLabel" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="addressLabel" class="form-label">Project Address:</label>
+                            <input type="text" class="form-control" id="addressLabel" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="leadLabel" class="form-label">Lead:</label>
+                            <input type="text" class="form-control" id="leadLabel" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="crewAssignLabel" class="form-label">Crew Assigned:</label>
+                            <input type="text" class="form-control" id="crewAssignLabel" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="tasks" class="form-label">Break Tasks Into Steps:</label>
+                            <textarea class="form-control" id="textarea" rows="3" required></textarea>
+                        </div>
+                        <div class="mb-3">
+                        <label for="q1" class="form-label">Can any body part get caught in, struckby, or caught between objects?</label>
+                        <br>
+                        <select class="form-control" required>
+                            <option style="display: none;">-- Select your answer --</option>
+                            <option value="no">No</option>
+                            <option value="yes">Yes</option>
+                        </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="q2" class="form-label">Is the employee working with sharp or rough materials that require PPE?</label>
+                            <br>
+                            <select class="form-control" required>
+                                <option style="display: none;">-- Select your answer --</option>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="q3" class="form-label">Can pushing, pulling, lifting, bending, or twisting cause strain?</label>
+                            <br>
+                            <select class="form-control" required>
+                                <option style="display: none;">-- Select your answer --</option>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="q4" class="form-label">Do tools, machines, or equipment present any harm?</label>
+                            <br>
+                            <select class="form-control" required>
+                                <option style="display: none;">-- Select your answer --</option>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="q5" class="form-label">Can the worker slip, trip, or fall?</label>
+                            <br>
+                            <select class="form-control" required>
+                                <option style="display: none;">-- Select your answer --</option>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="q6" class="form-label">Is heavy equipment needed?</label>
+                            <br>
+                            <select class="form-control" required>
+                                <option style="display: none;">-- Select your answer --</option>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="q7" class="form-label">Are there flammable, explosive, or electrical hazards present?</label>
+                            <br>
+                            <select class="form-control" required>
+                                <option style="display: none;">-- Select your answer --</option>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="q8" class="form-label">Is there a danger from falling objects?</label>
+                            <br>
+                            <select class="form-control" required>
+                                <option style="display: none;">-- Select your answer --</option>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="q9" class="form-label">Can weather conditions affect Safety?</label>
+                            <br>
+                            <select class="form-control" required>
+                                <option style="display: none;">-- Select your answer --</option>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="q10" class="form-label">Contact with acids, toxins, or caustics? (MSDS)</label>
+                            <br>
+                            <select class="form-control" required>
+                                <option style="display: none;">-- Select your answer --</option>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="hazardsTextArea" class="form-label">Hazards Associated With Steps:</label>
+                            <textarea class="form-control" id="textarea" rows="3" required></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="safePPE" class="form-label">Safe Work Procedures/PPE:</label>
+                            <textarea class="form-control" id="textarea" rows="3" required></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="nameLabel" class="form-label">Name:</label>
+                            <input type="text" class="form-control" id="nameLabel" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
                 </div>
             </div>
         </div>
--- resources/views/pages/safety/forms/form_blades/Vehicle Inspection.blade.php
+++ resources/views/pages/safety/forms/form_blades/Vehicle Inspection.blade.php
@@ -7,7 +7,7 @@
             <div class="card mt-3">
                 <div class="card-header">
                     <a class="" href="{{ route('safety.forms.forms_index') }}"><button class="btn btn-primary text-light">Back</button></a>
-                    <span class="float-end py-auto"><strong>Field Safety Observation</strong></span>
+                    <span class="float-end py-auto"><strong>Vehicle Inspection Report</strong></span>
                 </div>
 
                 <div class="card-body">
@@ -16,7 +16,192 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    
+                    <div class="container">
+        <h1>
+            Vehicle Inspection Report
+        </h1>
+        <form>
+            <div class="mb-3">
+                <label for="dateLabel" class="form-label">Date</label>
+                <input type="date" class="form-control" id="dateLabel" required>
+            </div>
+            <div class="mb-3">
+                <label for="truck" class="form-label">Truck:</label>
+                <br>
+                <select class="form-select" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">105 - F250</option>
+                    <option value="2">106 - F250 Ladder Rack</option>
+                    <option value="3">323 - Chevy</option>
+                    <option value="4">332 - Flatbed</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="trailer" class="form-label">Trailer:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">T-14DT - Flat</option>
+                </select>
+            </div>
+            <div class="mb-3">
+              <label for="equipmentLabel" class="form-label">Equipment:</label>
+              <input type="text" class="form-control" id="equipmentLabel" required>
+            </div>
+            <div class="mb-3">
+                <label for="startingODOLabel" class="form-label">Starting Odometer Mileage:</label>
+                <input type="text" class="form-control" id="startingODOLabel" required>
+            </div>
+            <div class="mb-3">
+                <label for="endingODOLabel" class="form-label">Ending Odometer Mileage:</label>
+                <input type="text" class="form-control" id="endingODOLabel" required>
+            </div>
+            <div class="mb-3">
+                <label for="engine" class="form-label">Engine:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Unsatisfactory</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="clutch" class="form-label">Clutch:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Unsatisfactory</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="transmission" class="form-label">Transmission:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Unsatisfactory</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="steering" class="form-label">Steering Mechanism:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Unsatisfactory</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="horn" class="form-label">Horn:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Unsatisfactory</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="mirrors" class="form-label">Rear View Mirrors:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Unsatisfactory</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="lights" class="form-label">Lights and Reflectors:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Needs Replaced</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="wipers" class="form-label">Windshield Wipers and Washers:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Needs Replaced</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="parkingBreak" class="form-label">Parking Break:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Unsatisfactory</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="serviceBreaks" class="form-label">Service Breaks:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Unsatisfactory</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="tires" class="form-label">Tires:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Unsatisfactory</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="rims" class="form-label">Wheels and Rims:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Unsatisfactory</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="emergency" class="form-label">Emergency Equipment:</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">Satisfactory</option>
+                    <option value="2">Unsatisfactory</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="notes" class="form-label">Notes:</label>
+                <textarea class="form-control" id="textarea" rows="3" required></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="vehicleSafe" class="form-label">Is vehicle safe to drive?</label>
+                <br>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">No</option>
+                    <option value="2">Yes</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="acknowledgementLabel" class="form-label">Driver Responsibility & Acknowledgement:</label>
+                <p>As Driver, I am acknowledging that all Company owned & rented equipment is to be used solely for SCP, LLC use.  I understand in entirety that a Dishonest report or the occurrence of an incident due to negligence may be subject to discipline, up to and including termination.  By saying "Yes" I am accepting responsibility, and confirm that all statements and remarks reported are accurate.</p>
+                <select class="form-control" required>
+                    <option style="display: none;">-- Select your answer --</option>
+                    <option value="1">No</option>
+                    <option value="2">Yes</option>
+                </select>
+              </div>
+            <div class="mb-3">
+                <label for="nameLabel" class="form-label">Name:</label>
+                <input type="text" class="form-control" id="nameLabel" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
                     
                     <div class="container">
                         Form content here
