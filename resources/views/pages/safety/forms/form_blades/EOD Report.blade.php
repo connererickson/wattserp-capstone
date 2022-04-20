0866e35f88ea9853a3357f73338fe4252803add1
CollinWood12 <cjwood2000@gmail.com>
2022-04-19 22:32:08 -0700

    eod report history

--- resources/views/pages/safety/forms/form_blades/EOD Report.blade.php
+++ resources/views/pages/safety/forms/form_blades/EOD Report.blade.php
@@ -29,15 +29,15 @@
                         {{ csrf_field() }}
                             <div class="mb-3">
                               <label for="dateLabel" class="form-label">Date</label>
-                              <input name="date" type="date" class="form-control" id="dateLabel" required>
+                              <input name="date" type="date" class="form-control" id="dateLabel" value='<?php if(isset($form_data)) echo $form_data[0]->date; ?>' required>
                             </div>
                             <div class="mb-3">
                               <label for="jobsiteClean" class="form-label">Is jobsite clean?</label>
                               <br>
                               <select name="cleanJobsite" class="form-select" required>
                                 <option style="display: none;">-- Select your answer --</option>
-                                  <option value="no">No</option>
-                                  <option value="yes">Yes</option>
+                                  <option value="no" <?php if(isset($form_data) && strcmp($form_data[0]->jobsiteClean,"no")==0) echo 'selected';?>>No</option>
+                                  <option value="yes" <?php if(isset($form_data) && strcmp($form_data[0]->jobsiteClean,"yes")==0) echo 'selected';?>>Yes</option>
                               </select>
                             </div>
                             <div class="mb-3">
@@ -45,27 +45,29 @@
                                 <br>
                                 <select name="looseMaterials" class="form-select" required>
                                     <option style="display: none;">-- Select your answer --</option>
-                                    <option value="no">No</option>
-                                    <option value="yes">Yes</option>
+                                    <option value="no" <?php if(isset($form_data) && strcmp($form_data[0]->looseMaterialsScrewed,"no")==0) echo 'selected';?>>No</option>
+                                    <option value="yes" <?php if(isset($form_data) && strcmp($form_data[0]->looseMaterialsScrewed,"yes")==0) echo 'selected';?>>Yes</option>
                                 </select>
                             </div>
                             <div class="mb-3">
                                 <label for="tasks" class="form-label">What tasks were completed today?</label>
-                                <textarea name="completedTasks" class="form-control" id="textarea" rows="3" required></textarea>
+                                <textarea name="completedTasks" class="form-control" id="textarea" rows="3" required><?php if(isset($form_data)) echo $form_data[0]->tasksCompleted; ?></textarea>
                             </div>
                             <div class="mb-3">
                                 <label for="parts" class="form-label">Do you need parts to be ordered?</label>
-                                <textarea name="partsToBeOrdered" class="form-control" id="textarea" rows="3" required></textarea>
+                                <textarea name="partsToBeOrdered" class="form-control" id="textarea" rows="3" required><?php if(isset($form_data)) echo $form_data[0]->partsToOrder; ?></textarea>
                             </div>
                             <div class="mb-3">
                                 <label for="notes" class="form-label">Notes:</label>
-                                <textarea name="notes" class="form-control" id="textarea" rows="3" required></textarea>
+                                <textarea name="notes" class="form-control" id="textarea" rows="3" required><?php if(isset($form_data)) echo $form_data[0]->notes; ?></textarea>
                             </div>
+                            @if(!isset($form_data))
                             <div class="row">
                                 <div class="col text-center">
                                     <button type="submit" class="btn btn-primary w-25 text-light">Submit</button>
                                 </div>
                             </div>
+                            @endif
                         </form>
                     </div>
                 </div>
